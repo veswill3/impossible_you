c582d389542bd77238a4f6a92ce299269eeb0a23
Nolan Johnson <nolanbjohnson@gmail.com>
2013-03-02 18:00:04 -0600

    fixed some paths

--- spec/requests/goals_spec.rb
+++ spec/requests/goals_spec.rb
@@ -4,7 +4,7 @@ describe "Goals" do
   describe "GET /goals" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get goals_path
+      get list_goals_path
       response.status.should be(200)
     end
   end
