09252d9a6d555bc8d08cdcddf491459db954b8c1
Vesper Williams <veswill3@gmail.com>
2013-03-17 13:30:37 -0500

    updated routing spec for goals to close issue #10

--- spec/routing/goals_routing_spec.rb
+++ spec/routing/goals_routing_spec.rb
@@ -4,31 +4,31 @@ describe GoalsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/goals").should route_to("goals#index")
+      get("users/1/goals").should route_to("goals#index", user_id: "1")
     end
 
     it "routes to #new" do
-      get("/goals/new").should route_to("goals#new")
+      get("users/1/goals/new").should route_to("goals#new", user_id: "1")
     end
 
     it "routes to #show" do
-      get("/goals/1").should route_to("goals#show", :id => "1")
+      get("/users/1/goals/1").should route_to("goals#show", user_id: "1", id: "1")
     end
 
     it "routes to #edit" do
-      get("/goals/1/edit").should route_to("goals#edit", :id => "1")
+      get("/users/1/goals/1/edit").should route_to("goals#edit", user_id: "1", id: "1")
     end
 
     it "routes to #create" do
-      post("/goals").should route_to("goals#create")
+      post("/users/1/goals").should route_to("goals#create", user_id: "1")
     end
 
     it "routes to #update" do
-      put("/goals/1").should route_to("goals#update", :id => "1")
+      put("/users/1/goals/1").should route_to("goals#update", user_id: "1", id: "1")
     end
 
     it "routes to #destroy" do
-      delete("/goals/1").should route_to("goals#destroy", :id => "1")
+      delete("/users/1/goals/1").should route_to("goals#destroy", user_id: "1", id: "1")
     end
 
   end
