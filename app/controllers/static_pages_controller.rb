3f5fa2c4465fad2ba0cfbf5c31aa140ecbc1ccfb
Vesper Williams <veswill3@gmail.com>
2013-03-17 18:00:37 -0500

    updated request spec for goals to close issue #12 goals are now listed on home page if signed in created a place holder for Goal feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if signed_in?
+      @user  = current_user
+      @goals = current_user.goals.paginate(page: params[:page])
+    end
   end
 
   def about
