2afa5310ed7be6ab36a8b6c00747826ef903dccf
Vesper Williams <veswill3@gmail.com>
2013-03-23 00:48:53 -0500

    a few more tweaks and cleanup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,15 +11,17 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:goals) }
+  it { should respond_to(:admin) }
 
   it { should be_valid }
   it { should_not be_admin }
 
-  describe "with admin attributes set to 'true'" do
+  describe "with admin attribute set to 'true'" do
     before do
       @user.save!
       @user.toggle!(:admin)
@@ -29,7 +31,7 @@ describe User do
   end
 
   describe "when name is not present" do
-    before { @user.name = "" }
+    before { @user.name = " " }
     it { should_not be_valid }
   end
 
@@ -43,7 +45,7 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when email format is valid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -65,13 +67,23 @@ describe User do
   end
 
   describe "when email address is already taken" do
-    before do      
+    before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
 
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
@@ -93,8 +105,8 @@ describe User do
      before { @user.password = @user.password_confirmation = "a" * 3 }
      it { should be_invalid }
   end
-    
-  describe "return value of authentication method" do
+
+  describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user) { User.find_by_email(@user.email) }
 
@@ -104,8 +116,7 @@ describe User do
 
     describe "with invalid password" do
     let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-      
-        
+
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
